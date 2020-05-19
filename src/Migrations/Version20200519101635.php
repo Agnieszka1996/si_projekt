160ab8df57738bfe7e6a890832c1d448786b9428
Agnieszka Bień <agnieszka.bien@virtual.com>
2020-05-19 13:01:34 +0200

    First entities

--- src/Migrations/Version20200519101635.php
+++ src/Migrations/Version20200519101635.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200519080122 extends AbstractMigration
+final class Version20200519101635 extends AbstractMigration
 {
     public function getDescription() : string
     {
